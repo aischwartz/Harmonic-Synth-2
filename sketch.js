29ae8a869bcf702d3e03b5d43506b8632f194c88
Adam Schwartz <aischwartz@towson.edu>
2019-08-08 10:52:03 -0400

    Added 2nd oscillator
    dials and toggles all on top of eachother.  Tried to fix in CSS

--- sketch.js
+++ sketch.js
@@ -1,22 +1,36 @@
 
-// let synth = new Nexus.Rack("#synth")
-
-let power = new Nexus.Toggle("#power1");
-let gain = new Nexus.Dial("#gain1",{
+"Osc1"
+let power1 = new Nexus.Toggle("#power1");
+let gain1 = new Nexus.Dial("#gain1",{
   "size": [75,75],
   "interaction": "vertical",
   "min": 0,
   "max": 1
 });
-let freq = new Nexus.Dial("#frequency1",{
+let freq1 = new Nexus.Dial("#frequency1",{
   "interaction": "vertical",
   "min": 100,
   "max": 10000
 })
-// let power2 = new Nexus.Toggle("#power2");
-// let gain2 = new Nexus.Slider("#gain2");
+
+"Osc2"
+let power2 = new Nexus.Toggle("#power2");
+let gain2 = new Nexus.Dial("#gain2",{
+  "size": [75,75],
+  "interaction": "vertical",
+  "min": 0,
+  "max": 1
+});
+let freq2 = new Nexus.Dial("#frequency2",{
+  "interaction": "vertical",
+  "min": 100,
+  "max": 10000
+})
+
 
 let osc;
+let osc1;
+let osc2;
 
 function setup(){
 
@@ -31,7 +45,7 @@ function setup(){
 
 
   // Listen for interface events
-  power.on('change',function(v) {
+  power1.on('change',function(v) {
   // console.log(v)
   if(v == true){
     osc.start()
@@ -41,16 +55,43 @@ function setup(){
   }
   });
 
-  gain.on('change',function(v) {
+  gain1.on('change',function(v) {
   // console.log(v)
   osc.amp(v)
   });
 
-  freq.on('change',function(v) {
-  console.log(v)
+  freq1.on('change',function(v) {
+  // console.log(v)
+  osc.freq(v)
+  });
+
+
+  osc = new p5.Oscillator();
+  osc.setType("square");
+  osc.freq(100);
+  osc.amp(0);
+
+  power2.on('change',function(v) {
+  // console.log(v)
+  if(v == true){
+    osc.start()
+  }
+  if(v == false){
+    osc.stop()
+  }
+  });
+
+  gain2.on('change',function(v) {
+  // console.log(v)
+  osc.amp(v)
+  });
+
+  freq2.on('change',function(v) {
+  // console.log(v)
   osc.freq(v)
   });
 }
+
 
   // function draw(){
   //   print(mouseX, mouseY);
