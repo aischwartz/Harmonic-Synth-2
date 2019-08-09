435984e1ea2347cec7fc604b9838edf0ab1c0481
Adam Schwartz <aischwartz@towson.edu>
2019-08-09 10:11:52 -0400

    Colorized

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 
+
 "Osc1"
 let power1 = new Nexus.Toggle("#power1");
 let gain1 = new Nexus.Dial("#gain1",{
@@ -13,6 +14,12 @@ let freq1 = new Nexus.Dial("#frequency1",{
   "min": 100,
   "max": 10000
 })
+power1.colorize("accent","#ff0")
+power1.colorize("fill","#555")
+gain1.colorize("accent","#ff0")
+gain1.colorize("fill","#555")
+freq1.colorize("accent","#ff0")
+freq1.colorize("fill","#555")
 
 "Osc2"
 let power2 = new Nexus.Toggle("#power2");
@@ -28,11 +35,43 @@ let freq2 = new Nexus.Dial("#frequency2",{
   "min": 100,
   "max": 10000
 })
+power2.colorize("accent","#11f01c")
+power2.colorize("fill","#555")
+gain2.colorize("accent","#1f1")
+gain2.colorize("fill","#555")
+freq2.colorize("accent","#1f1")
+freq2.colorize("fill","#555")
+
+"Osc3"
+let power3 = new Nexus.Toggle("#power3");
+let gain3 = new Nexus.Dial("#gain3",{
+  "size": [75,75],
+  "interaction": "vertical",
+  "min": 0,
+  "max": 1
+});
+
+let freq3 = new Nexus.Dial("#frequency3",{
+  "interaction": "vertical",
+  "min": 100,
+  "max": 10000
+})
+
+power3.colorize("accent","#f11")
+power3.colorize("fill","#555")
+gain3.colorize("accent","#f11")
+gain3.colorize("fill","#555")
+freq3.colorize("accent","#f11")
+freq3.colorize("fill","#555")
+
+document.body.style.backgroundColor = "#000";
 
 
-let osc;
+
+// let osc;
 let osc1;
 let osc2;
+let osc3;
 
 function setup(){
 
@@ -42,7 +81,7 @@ function setup(){
 // OSCILLATOR 1
   osc1 = new p5.Oscillator();
   osc1.setType("sine");
-  osc1.freq(100);
+  osc1.freq(220);
   osc1.amp(0);
 
   // Listen for interface events
@@ -70,7 +109,7 @@ function setup(){
 // OSCILLATOR 2
   osc2 = new p5.Oscillator();
   osc2.setType("square");
-  osc2.freq(100);
+  osc2.freq(440);
   osc2.amp(0);
 
   power2.on('change',function(v) {
@@ -91,6 +130,32 @@ function setup(){
   freq2.on('change',function(v) {
     // console.log(v)
     osc2.freq(v)
+  });
+
+// OSCILLATOR 2
+  osc3 = new p5.Oscillator();
+  osc3.setType("triangle");
+  osc3.freq(880);
+  osc3.amp(0);
+
+  power3.on('change',function(v) {
+  // console.log(v)
+    if(v == true){
+      osc3.start()
+    }
+    if(v == false){
+      osc3.stop()
+    }
+  });
+
+  gain3.on('change',function(v) {
+  // console.log(v)
+    osc3.amp(v)
+  });
+
+  freq3.on('change',function(v) {
+    // console.log(v)
+    osc3.freq(v)
   });
 }
 
